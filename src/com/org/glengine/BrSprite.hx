a1425a1b908220a4712cbdc4a5f80965a9890372
dreamycactus <hello_great_man@yahoo.ca>
2015-01-06 15:34:24 -0500

    small fix for sprite

--- src/com/org/glengine/BrSprite.hx
+++ src/com/org/glengine/BrSprite.hx
@@ -10,14 +10,14 @@ class BrSprite extends TextureRegion implements BrDrawable
     static var VERTEX_SIZE = 2 + 2 + 1;
     static var SPRITE_SIZE = 4 * VERTEX_SIZE;
     
-    public var x : Float;
-    public var y : Float;
-    public var originX : Float = 0;
-    public var originY : Float = 0;
-    public var rotation : Float = 0;
-    public var scaleX : Float = 1;
-    public var scaleY : Float = 1;
-    public var vertices(get, default) : Float32Array = new Float32Array(SPRITE_SIZE);
+    public var x(default, set) : Float;
+    public var y(default, set) : Float;
+    public var originX(default, set) : Float = 0;
+    public var originY(default, set) : Float = 0;
+    public var rotation(default, set) : Float = 0;
+    public var scaleX(default, set) : Float = 1;
+    public var scaleY(default, set) : Float = 1;
+    public var vertices(get, null) : Float32Array = new Float32Array(SPRITE_SIZE);
 
     
     var dirty : Bool = false;
@@ -43,5 +43,54 @@ class BrSprite extends TextureRegion implements BrDrawable
     }
     
     function get_vertices() : Float32Array
+    {
+        if (dirty) {
+            dirty = false;
+        }
+        return vertices;
+    }
+    
+    function set_x(x : Float) : Float
+    {
+        dirty = true;
+        this.x = x;
+        return x;
+    }
+    function set_y(y : Float) : Float
+    {
+        dirty = true;
+        this.y = y;
+        return y;
+    }
+    function set_rotation(r : Float) : Float
+    {
+        dirty = true;
+        this.rotation = r;
+        return r;
+    }
+    function set_originX(ox : Float) : Float
+    {
+        dirty = true;
+        this.originX = ox;
+        return ox;
+    }
+    function set_originY(oy : Float) : Float
+    {
+        dirty = true;
+        this.originY = oy;
+        return oy;
+    }
+    function set_scaleX(sx : Float) : Float
+    {
+        dirty = true;
+        this.scaleX = sx;
+        return sx;
+    }
+    function set_scaleY(sy : Float) : Float
+    {
+        dirty = true;
+        this.scaleY = sy;
+        return sy;
+    }
     
 }
